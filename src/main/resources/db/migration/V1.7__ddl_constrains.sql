ae8cb3284be60e18bd7f680556152bad25b7398b
Florian Dodaj <floriandodaj55@gmail.com>
2022-04-17 16:20:03 +0200

    fix flyway

--- src/main/resources/db/migration/V1.7__ddl_constrains.sql
+++ src/main/resources/db/migration/V1.7__ddl_constrains.sql
@@ -1,6 +1,6 @@
-alter table `userEntity`  add constraint `user_role_fk` foreign key (`user_role`) references  `roleEntity` (`id`);
-alter table  `request_permission` add constraint `user_request_fk` foreign key (`userEntity`) references  `userEntity` (`id`);
-alter table  `department` add constraint `department_request_fk` foreign key (`department_leader`) references  `userEntity` (`id`);
+alter table `user`  add constraint `user_role_fk` foreign key (`user_role`) references  `role` (`id`);
+alter table  `request_permission` add constraint `user_request_fk` foreign key (`userEntity`) references  `user` (`id`);
+alter table  `department` add constraint `department_request_fk` foreign key (`department_leader`) references  `user` (`id`);
 
-alter table `user_department` add constraint `user_department_mtm` foreign key (`user_id`) references `userEntity` (`id`);
+alter table `user_department` add constraint `user_department_mtm` foreign key (`user_id`) references `user` (`id`);
 alter table `user_department` add constraint `user_department_mtom` foreign key  (`department_id`) references `department` (`id`);
