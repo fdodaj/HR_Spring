8198d892d5998c1f5a2f381075e991ff878b3c62
Florian Dodaj <floriandodaj55@gmail.com>
2022-04-21 20:41:35 +0200

    init

--- src/main/java/al/ikubinfo/hrmanagement/security/ApplicationSecurityConfig.java
+++ src/main/java/al/ikubinfo/hrmanagement/security/ApplicationSecurityConfig.java
@@ -1,8 +1,21 @@
 package al.ikubinfo.hrmanagement.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig  extends WebSecurityConfigurerAdapter {
 
-public class ApplicationSecurityConfig {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
 }
