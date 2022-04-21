59b4b745180cf12f6177be631697a1ce82a5d0ad
Florian Dodaj <floriandodaj55@gmail.com>
2022-04-21 21:06:14 +0200

    test

--- src/main/java/al/ikubinfo/hrmanagement/repository/RequestRepository.java
+++ src/main/java/al/ikubinfo/hrmanagement/repository/RequestRepository.java
@@ -4,6 +4,7 @@ import al.ikubinfo.hrmanagement.entity.RequestEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface RequestRepository extends JpaRepository<RequestEntity, Long> {
 }
